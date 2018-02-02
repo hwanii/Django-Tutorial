01cc5c3b441fcffce7ca84c2d6c9a451bd549289
Ryu Taehwan <yth3260@gmail.com>
2018-02-02 15:28:12 +0900

    detail view에 Choice 목록을 출력

--- django/polls/views.py
+++ django/polls/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 from polls.models import Question
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
+    question = Question.objects.get(pk=question_id)
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -23,3 +27,5 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return render(request, 'polls/index.html', context)
+    # template = loader.get_template('poll/index.html')
+    # return HttpResponse(template.render(context, request))
